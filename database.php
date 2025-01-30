6f5b47f8fd32f691eafa82d4f9e6aae311606c0b
FarazAhmad-117 <farazahmad31048@gmail.com>
2025-01-30 23:43:30 +0500

    some learning

---
+++ database.php
@@ -0,0 +1,27 @@
+<?php
+
+
+$db_server = "localhost";
+$db_user = "root";
+$db_password = "";
+$db_name = "business_db";
+
+$conn = "";
+
+
+try {
+    $conn = mysqli_connect(
+        $db_server,
+        $db_user,
+        $db_password,
+        $db_name
+    );
+} catch (mysqli_sql_exception) {
+    echo "Error connecting to Database!";
+}
+
+if ($conn) {
+    echo "Connection established!";
+}
+
+?>
---
+++ db.php
@@ -0,0 +1,19 @@
+<?php
+
+// Just learning how to use php databases
+include("database.php");
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Db Configuration</title>
+</head>
+<body>
+    <h2>Hello Page</h2>
+</body>
+</html>
